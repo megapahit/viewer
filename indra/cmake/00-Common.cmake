75012c4f0c0b852c8bfa0b5632bf82c82f240754
Erik Kundiman <erik@megapahit.org>
2025-06-19 17:48:03 +0800

    GLM_FORCE_NEON for Windows arm64 too
    though there are still so many compile errors, but it should be in
    the right direction, cause firstly we can't let it use GLM_FORCE_SSE2
    otherwise it would include x/emmintrin headers, and secondly, if
    none of the GLM_FORCEs is used then the errors are much fewer but
    related to __m128 which should be translatable by SSE2NEON (that's
    why I believe GLM_FORCE_NEON should be ON for Windows arm64 too).

--- indra/cmake/00-Common.cmake
+++ indra/cmake/00-Common.cmake
@@ -33,7 +33,7 @@ add_compile_definitions( ADDRESS_SIZE=${ADDRESS_SIZE})
 # -- which we do. Without one or the other, we get a ton of Boost warnings.
 add_compile_definitions(BOOST_BIND_GLOBAL_PLACEHOLDERS)
 
-if(CMAKE_OSX_ARCHITECTURES MATCHES arm64 OR CMAKE_SYSTEM_PROCESSOR MATCHES aarch64)
+if(CMAKE_OSX_ARCHITECTURES MATCHES arm64 OR CMAKE_SYSTEM_PROCESSOR MATCHES aarch64 OR ($ENV{MSYSTEM_CARCH} MATCHES aarch64))
 add_compile_definitions(GLM_FORCE_DEFAULT_ALIGNED_GENTYPES=1 GLM_FORCE_NEON=1 GLM_ENABLE_EXPERIMENTAL=1)
 else()
 # Force enable SSE2 instructions in GLM per the manual
