megapahit/viewer

commit d0e3c7a40395ea5ad7f7a9982ce4dbf464e24caa

Minimal @unsit=<y/n> command implementation
Hides the Stand up button too, but doesn't prevent teleporting yet.

indra/newview/rlvhandler.cpp
@@ -28,6 +28,7 @@
 #include "llviewerprecompiledheaders.h"
 #include "llagent.h"
 #include "llstartup.h"
+#include "llmoveview.h"
 #include "llviewercontrol.h"
 #include "llviewermenu.h"
 #include "llviewerobject.h"
@@ -267,4 +268,14 @@ ECmdRet CommandHandlerBaseImpl<EParamType::AddRem>::processCommand(const RlvComm
     return (*pHandler)(rlvCmd, toggle);
 }
 
+template<> template<>
+ECmdRet BehaviourToggleHandler<EBehaviour::Unsit>::onCommand(const RlvCommand& rlvCmd, bool& toggle)
+{
+    gAgent.setAllowedToStand(toggle);
+    if (gAgent.isSitting())
+        LLPanelStandStopFlying::getInstance()->setVisibleStandButton(toggle);
+    return ECmdRet::Succeeded;
+}
+
+template<> template<>
 // ============================================================================

indra/newview/llagent.cpp
@@ -386,6 +386,8 @@ LLAgent::LLAgent() :
     mbRunning(false),
     mbTeleportKeepsLookAt(false),
 
+    mAllowedToStand(true),
+
     mAgentAccess(new LLAgentAccess(gSavedSettings)),
     mGodLevelChangeSignal(),
     mCanEditParcel(false),
@@ -969,7 +971,8 @@ bool LLAgent::isSitting()
 
 void LLAgent::standUp()
 {
-    setControlFlags(AGENT_CONTROL_STAND_UP);
+    if (mAllowedToStand)
+        setControlFlags(AGENT_CONTROL_STAND_UP);
 }
 
 void LLAgent::changeParcels()

indra/newview/llagent.h
@@ -446,6 +446,10 @@ public:
     void            standUp();
     /// @brief ground-sit at agent's current position
     void            sitDown();
+    bool            isAllowedToStand() const      { return mAllowedToStand; }
+    void            setAllowedToStand(bool allow) { mAllowedToStand = allow; }
+private:
+    bool            mAllowedToStand;
 
     //--------------------------------------------------------------------
     // Do Not Disturb

indra/newview/llmoveview.cpp
@@ -538,7 +538,7 @@ void LLPanelStandStopFlying::setStandStopFlyingMode(EStandStopFlyingMode mode)
         LLFirstUse::sit();
         LLFirstUse::notMoving(false);
     }
-    panel->mStandButton->setVisible(SSFM_STAND == mode);
+    panel->mStandButton->setVisible((SSFM_STAND == mode) & gAgent.isAllowedToStand());
     panel->mStopFlyingButton->setVisible(SSFM_STOP_FLYING == mode);
 
     //visibility of it should be updated after updating visibility of the buttons

indra/newview/llmoveview.h
@@ -143,6 +143,7 @@ public:
     static void clearStandStopFlyingMode(EStandStopFlyingMode mode);
     /*virtual*/ bool postBuild();
     /*virtual*/ void setVisible(bool visible);
+    void setVisibleStandButton(bool visible) { mStandButton->setVisible(visible); }
 
     // *HACK: due to hard enough to have this control aligned with "Move" button while resizing
     // let update its position in each frame

indra/newview/rlvhelper.cpp
@@ -60,6 +60,7 @@ BehaviourDictionary::BehaviourDictionary()
     addEntry(new ForceProcessor<EBehaviour::Unsit>("unsit"));
 
     // AddRem
+    addEntry(new BehaviourProcessor<EBehaviour::Unsit>("unsit"));
 
     // Populate mString2InfoMap (the tuple <behaviour, type> should be unique)
     for (const BehaviourInfo* bhvr_info_p : mBhvrInfoList)

commit 71c8547184458fb5b2a79cec28787a6999cad4a6

Fix commit d0e3c7a40395ea5ad7f7a9982ce4dbf464e24caa
Accidentally git added a line that wasn't meant to be staged yet.

indra/newview/rlvhandler.cpp
@@ -294,5 +294,4 @@ ECmdRet BehaviourToggleHandler<EBehaviour::Unsit>::onCommand(const RlvCommand& r
     return ECmdRet::Succeeded;
 }
 
-template<> template<>
 // ============================================================================