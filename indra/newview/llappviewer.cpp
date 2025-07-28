c1adf320af2ca2afd9ed05cccc9031b99fc7ee60
Secret Foxtail <remmy@megapahit.net>
2025-07-28 05:23:18 -0600

    Discord Rich Presence: Hide name & connect to llappviewer.cpp
    Add option to show/hide avatar name in privacy panel & connect rich presense directly to llappviewer.cpp

--- indra/newview/llappviewer.cpp
+++ indra/newview/llappviewer.cpp
@@ -5938,10 +5938,10 @@ void LLAppViewer::initDiscordSocial()
     }
 }
 
-void LLAppViewer::handleDiscordSocial(bool enable)
+void LLAppViewer::handleDiscordSocial(const LLSD& value)
 {
     static const uint64_t APPLICATION_ID = 1393451183741599796;
-    if (enable)
+    if (value.asBoolean())
     {
         discordpp::AuthorizationArgs args{};
         args.SetClientId(APPLICATION_ID);
@@ -5986,13 +5986,16 @@ void LLAppViewer::updateDiscordActivity()
     discordpp::Activity activity;
     activity.SetType(discordpp::ActivityTypes::Playing);
 
-    LLAvatarName av_name;
-    LLAvatarNameCache::get(gAgent.getID(), &av_name);
-    auto name = av_name.getUserName();
-    auto displayName = av_name.getDisplayName();
-    if (name != displayName)
-        name = displayName + " (" + name + ")";
-    activity.SetDetails(name);
+    if (gSavedSettings.getBOOL("ShowDiscordActivityDetails"))
+    {
+        LLAvatarName av_name;
+        LLAvatarNameCache::get(gAgent.getID(), &av_name);
+        auto name = av_name.getUserName();
+        auto displayName = av_name.getDisplayName();
+        if (name != displayName)
+            name = displayName + " (" + name + ")";
+        activity.SetDetails(name);
+    }
 
     if (gSavedSettings.getBOOL("ShowDiscordActivityState"))
     {
